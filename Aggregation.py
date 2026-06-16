1c149790d898217fb364e253c3b234a4e9915790
Tanmay Janrao <tanmayjanrao.work@gmail.com>
2026-06-16 20:22:12 +0530

    Update Aggregation.py
    Removed unnecessary punctuation from comments for clarity.

--- Aggregation.py
+++ Aggregation.py
@@ -1,8 +1,8 @@
 # Aggregation is a concept in which an object of one class can own or access another independent object of another class. 
 
-## It represents Has-A’s relationship.
+## It represents Has-A’s relationship
 # It is a unidirectional association i.e. a one-way relationship. For example, a department can have students but vice versa is not possible and thus unidirectional in nature.
-# In Aggregation, both the entries can survive individually which means ending one entity will not affect the other entity.
+# In Aggregation, both the entries can survive individually which means ending one entity will not affect the other entity
 
 
 
