6f2694e978802fbdbe641254445a1af2cd43fca1
Tanmay Janrao <tanmayjanrao.work@gmail.com>
2026-06-30 23:54:45 +0530

    Update LLM_inheritance.py

--- LLM_inheritance.py
+++ LLM_inheritance.py
@@ -7,7 +7,10 @@ class LLM:
     def intro(self):
         return f"Welcome to your personal LLM {self.name}"
     
-   
+    def start(self):
+        return "Let's get you started"
+
 
 obj=LLM("Tanmay")
 print(obj.intro())
+print(obj.start())
