12b79605fe36df046946575b5f9f8a093975c599
Tanmay Janrao <tanmayjanrao.work@gmail.com>
2026-09-06 23:51:10 +0530

    Enhance Student class with name and intro method
    Refactor Student class to include name and improve methods.

--- Inheritance_2.py
+++ Inheritance_2.py
@@ -1,24 +1,32 @@
 class School:
 
-    def __init__(self,school_name):
-        self.school_name=school_name
+    def __init__(self, school_name):
+        self.school_name = school_name
         print("Parent constructor")
 
-  
 
     def show(self):
         return self.school_name
+
 
 class Student(School):
 
-    
+    def __init__(self, school_name, name):
+        super().__init__(school_name)
+        self.name = name
+
     def show(self):
         return self.school_name
-    
-    def __str__(self):  
+
+    def __str__(self):
         return f"My school is {self.school_name}"
 
+    def intro(self):
+        return f"My name is {self.name}."
 
-obj=Student("SRV")
+
+obj = Student("SRV", "Tanmay")
+
 print(obj)
 print(obj.show())
+print(obj.intro())
