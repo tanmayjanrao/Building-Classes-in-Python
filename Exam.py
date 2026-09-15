9864b4282517c90e3b76a003ec1564f133773c0b
Tanmay Janrao <tanmayjanrao.work@gmail.com>
2026-09-16 01:32:18 +0530

    Refactor Exam class for improved readability

--- Exam.py
+++ Exam.py
@@ -2,22 +2,22 @@ class Exam:
     __Exam_name = "JEE MAINS"
 
     def __init__(self, name, ID, standard):
-        self.__name=name
-        self.__ID=ID
-        self.standard=standard
+        self.__name = name
+        self.__ID = ID
+        self.standard = standard
 
     def get_name(self):
         return self.__name
     
     def set_change_name(self, new_name):
-         if type(new_name) == str:  
+        if type(new_name) == str:
             self.__name = new_name
             print(f"name is updated to {new_name}")
-         else:
-            raise Exception("Name must be a string!") 
+        else:
+            raise Exception("Name must be a string!")
 
     def is_eligible(self):
-    return self.standard >= 11
+        return self.standard >= 11
 
     @staticmethod
     def get_exam_name():
@@ -26,11 +26,11 @@ class Exam:
     def __str__(self):
         return f"Student: {self.__name}, ID: {self.__ID}, Standard: {self.standard}"
 
-student=Exam("Tanmay",67,10)
+
+student = Exam("Tanmay", 67, 10)
 print(student)
 
-print(Exam.get_exam_name())  
-
+print(Exam.get_exam_name())
 
 print(student.get_name())
 
@@ -38,9 +38,3 @@ student.set_change_name("Rahul")
 print(student.get_name())
 
 print(student.is_eligible())
-
-
-
-
-
-
